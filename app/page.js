807b7b36a0d5f05481bfb1ee860079cef95b50a4
Samir Nepal <samirnepal9817@gmail.com>
2024-08-20 12:53:00 +0545

    First commit

--- app/page.js
+++ app/page.js
@@ -2,112 +2,60 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex justify-center flex-col items-center w-[60vw] h-[40vh] gap-5 text-white">
+        <div className="text-5xl font-sans font-bold flex gap-2 items-center">Buy me a coffee <span><img src="/coffee.gif" width={55} alt="" /></span></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque quasi voluptates iure eos est sed </p>
+        <div>
+          {/* <button className="bg-white text-gray-800 px-4 py-2 rounded-lg font-semibold hover:bg-gray-200 hover:scale-[0.95] transition-[0.4s]">Start Now</button> */}
+          <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
+
+          <button type="button" class="text-white border-[1px] border-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 hover:border-slate-950">Read More</button>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="bg-white opacity-10 h-1 w-full">
+      </div>
+      
+      <div className="text-white container mx-auto">
+        <h1 className="text-center text-2xl my-12 font-bold">People can buy you a coffee</h1>
+        <div className="flex justify-around w-full mb-8">
+          <div className="item flex flex-col items-center gap-2">
+            <img className="bg-slate-100 p-3 rounded-full" src="/person-working.png" width={90} alt="" />
+            <p className="font-bold">Fund your projects</p>
+          </div>
+          <div className="item flex flex-col items-center gap-2">
+            <img className="bg-slate-100 p-3 rounded-full" src="/coins.gif" width={90} alt="" />
+            <p className="font-bold">Fund your projects</p>
+          </div>
+          <div className="item flex flex-col items-center gap-2">
+            <img className="bg-slate-100 p-3 rounded-full" src="/community.gif" width={90} alt="" />
+            <p className="font-bold">Fund your projects</p>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="bg-white opacity-10 h-1 w-full">
       </div>
-    </main>
+
+      <div className="text-white container mx-auto">
+        <h1 className="text-center text-2xl my-12 font-bold">Learn more about us</h1>
+        <div className="flex justify-around w-full">
+          <div className="item flex flex-col items-center gap-2">
+            <img className="bg-slate-100 p-3 rounded-full" src="/person-working.png" width={90} alt="" />
+            <p className="font-bold">Fund your projects</p>
+          </div>
+          <div className="item flex flex-col items-center gap-2">
+            <img className="bg-slate-100 p-3 rounded-full" src="/coins.gif" width={90} alt="" />
+            <p className="font-bold">Fund your projects</p>
+          </div>
+          <div className="item flex flex-col items-center gap-2">
+            <img className="bg-slate-100 p-3 rounded-full" src="/community.gif" width={90} alt="" />
+            <p className="font-bold">Fund your projects</p>
+          </div>
+        </div>
+      </div>
+    </>
+
   );
 }
---
+++ components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => {
+    return (
+        <nav class="bg-blue-950 shadow shadow-gray-300 w-100 px-8 md:px-auto">
+            <div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
+                <div class="text-white md:order-1">
+                   <span>getMea</span><span className='text-blue-500 font-bold'>COFFEE</span><span>!</span>
+                </div>
+                {/* <div class="text-white order-3 w-full md:w-auto md:order-2">
+                    <ul class="flex font-semibold justify-between">
+
+                        <li class="md:px-4 md:py-2 hover:text-blue-500 "><a href="#">Dashboard</a></li>
+                        <li class="md:px-4 md:py-2 hover:text-blue-500"><a href="#">Search</a></li>
+                        <li class="md:px-4 md:py-2 hover:text-blue-500"><a href="#">Explore</a></li>
+                        <li class="md:px-4 md:py-2 hover:text-blue-500"><a href="#">About</a></li>
+                        <li class="md:px-4 md:py-2 hover:text-blue-500"><a href="#">Contact</a></li>
+                    </ul>
+                </div> */}
+                <div class="order-2 md:order-3">
+                    <Link href={"/login"}>
+                    <button class="px-4 py-2 bg-white hover:bg-gray-200 text-gray-800 rounded-xl flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        </svg>
+                        <span>Login</span>
+                    </button>
+                    </Link>
+                    
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
